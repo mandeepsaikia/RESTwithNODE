3dac14ed4f046a394f4562ad4b8dbc148bfa1c86
Mandeep <saikia.mandeep@gmail.com>
2025-01-30 15:35:17 -0600

    Appended CORS_Headers

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const bodyparser = require('body-parser')
 app.use(bodyparser.json())
 
 const router = require('./routes/feed')
+/*
+   To handle requests to server from different domains i.e. CORS requests - we need to do setting on server side.
+   By default, CORS requests are blocked by browsers as a security measure. 
+   So, we need to set some CORS headers in the response headers i.e. leaving the server.
+   
+   app.use((req, res, next)=>{
+     res.setHeader('Access-Control-Allow-Origin','*')
+     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE')
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+     next();
+
+   })
+*/
+
 
 app.use('/feed', router)
 
